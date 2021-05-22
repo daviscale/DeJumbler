c20c92badd558d38c270fb6133026bf1424a6adf
Cale Davis <daviscale@gmail.com>
2021-05-22 05:25:32 -0400

    load word list in app

--- core/src/main/scala/org/daviscale/dejumbler/Dejumbler.scala
+++ core/src/main/scala/org/daviscale/dejumbler/Dejumbler.scala
@@ -1,6 +1,13 @@
 package org.daviscale.dejumbler
 
+import scala.io.Source
+
 object Dejumbler {
+
+  lazy val wordList: Seq[String] =
+    Source.fromResource("american-english-insane")
+      .getLines
+      .toSeq
 
   def getPermutations(word: String): Set[String] = {
     word
