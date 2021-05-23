8ab2c8a47183c86867548402d8f62a2ea6a6de82
Cale Davis <daviscale@gmail.com>
2021-05-23 07:55:17 -0400

    initial implementation of timings script

--- core/src/test/scala/org/daviscale/dejumbler/DejumblerTimings.scala
+++ core/src/test/scala/org/daviscale/dejumbler/DejumblerTimings.scala
@@ -1,5 +1,7 @@
 package org.daviscale.dejumbler
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import scala.util.Random
 import scala.util.chaining._
 
@@ -23,8 +25,37 @@ object DejumblerTimings {
       }
   }
 
-  def main(args: Array[String]): Unit = {
+  def getDejumbleTime(scrambledWord: String): Long = {
+    val start = System.nanoTime
+    Await.result(findCandidates(scrambledWord), 1.minute)
+    val finish = System.nanoTime
+    finish - start
+  }
 
+  def main(args: Array[String]): Unit = {
+    val numberWords = args.head.toInt
+    val scrambledWords = getScrambledWords(numberWords)
+    val timings = scrambledWords.map(getDejumbleTime).sorted
+    println(s"For sample size of $numberWords")
+    println(s"Average: ${average(timings)}")
+    println(s"Min: ${timings.head}")
+    (5 to 95)
+      .filter(_ % 5 == 0)
+      .foreach(tile => println(s"$tile-ile: ${percentile(timings, tile)}"))
+    println(s"Max: ${timings.last}")
+  }
+
+  def average(timings: Seq[Long]): Long = {
+    val sum = timings.reduce(_ + _)
+    sum / timings.size.toLong
+  }
+
+  def percentile(
+    sortedTimings: Seq[Long],
+    tile: Int
+  ):Long = {
+    val index = math.ceil((sortedTimings.length - 1) * (tile / 100.0)).toInt
+    sortedTimings(index)
   }
 
 }
